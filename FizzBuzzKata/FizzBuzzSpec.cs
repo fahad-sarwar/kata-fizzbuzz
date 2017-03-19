28d14c79476aee69da24c8092b8285da607d3a0e
fahad-sarwar <mohammed_fahad00@hotmail.com>
2017-03-19 18:23:52 +0000

    + Added in additional scenarios

--- FizzBuzzKata/FizzBuzzSpec.cs
+++ FizzBuzzKata/FizzBuzzSpec.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FizzBuzzKata
 {
@@ -6,17 +8,63 @@ namespace FizzBuzzKata
     public class FizzBuzzSpec
     {
         [TestMethod]
-        public void Number_Returns_Result()
+        public void Number_Returns_Result_Using_DefaultSubsitutions()
         {
-            Assert.AreEqual("0", FizzBuzzGame.Convert(0));
+            Assert.AreEqual("", new FizzBuzzGame().Convert(0));
+            Assert.AreEqual("1", new FizzBuzzGame().Convert(1));
+            Assert.AreEqual("Fizz", new FizzBuzzGame().Convert(3));
+            Assert.AreEqual("Buzz", new FizzBuzzGame().Convert(5));
+            Assert.AreEqual("FizzBuzz", new FizzBuzzGame().Convert(15));
+            Assert.AreEqual("Pop", new FizzBuzzGame().Convert(7));
+            Assert.AreEqual("FizzPop", new FizzBuzzGame().Convert(21));
+            Assert.AreEqual("BuzzPop", new FizzBuzzGame().Convert(35));
+            Assert.AreEqual("FizzBuzzPop", new FizzBuzzGame().Convert(105));
+            Assert.AreEqual("97", new FizzBuzzGame().Convert(97));
+        }
+
+        [TestMethod]
+        public void Number_Returns_Result_Using_CustomPreferences()
+        {
+            var _customPreference = new Dictionary<int, string>
+            {
+                { 2, "Fuzz" },
+                { 3, "Bizz" }
+            };
+
+            Assert.AreEqual("", new FizzBuzzGame(_customPreference).Convert(0));
+            Assert.AreEqual("1", new FizzBuzzGame(_customPreference).Convert(1));
+            Assert.AreEqual("Fuzz", new FizzBuzzGame(_customPreference).Convert(2));
+            Assert.AreEqual("Bizz", new FizzBuzzGame(_customPreference).Convert(9));
+            Assert.AreEqual("FuzzBizz", new FizzBuzzGame(_customPreference).Convert(12));
         }
     }
 
     public class FizzBuzzGame
     {
-        public static string Convert(int number)
+        private static readonly Dictionary<int, string> _defaultLookup = new Dictionary<int, string>
         {
-            return "0";
+            { 3, "Fizz" },
+            { 5, "Buzz" },
+            { 7, "Pop" }
+        };
+
+        private readonly Dictionary<int, string> _lookup = new Dictionary<int, string>();
+
+        public FizzBuzzGame(Dictionary<int, string> lookup = null)
+        {
+            _lookup = lookup ?? _defaultLookup;
+        }
+
+        public string Convert(int number)
+        {
+            if (number == 0)
+                return "";
+
+            return string.Join("", _lookup.Keys
+                .Where(key => number % key == 0)
+                .Select(key => _lookup[key])
+                .DefaultIfEmpty(number.ToString())
+                .ToList());
         }
     }
 }
