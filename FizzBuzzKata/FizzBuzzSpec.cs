f6d47f9a0ded0cda86e6743c2a104b96df61dea1
fahad-sarwar <mohammed_fahad00@hotmail.com>
2017-03-19 18:02:35 +0000

    + Created shell

---
+++ FizzBuzzKata/FizzBuzzSpec.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzKata
+{
+    [TestClass]
+    public class FizzBuzzSpec
+    {
+        [TestMethod]
+        public void Number_Returns_Result()
+        {
+            Assert.AreEqual("0", FizzBuzzGame.Convert(0));
+        }
+    }
+
+    public class FizzBuzzGame
+    {
+        public static string Convert(int number)
+        {
+            return "0";
+        }
+    }
+}
